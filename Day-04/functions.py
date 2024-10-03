08e884971a3b6ce562659d9bd874700ae3355f14
yamunasreeoggu <yamunasree321@gmail.com>
2024-10-03 13:26:40 -0400

    Practice | Yamuna | function program

---
+++ Day-04/functions.py
@@ -0,0 +1,20 @@
+a = 2
+b = 3
+
+def addition():
+    add = a + b
+    print("Addition:", add)
+
+def subtraction():
+    sub = a - b
+    print("Subtraction:", sub)
+
+def multiplication():
+    mul = a * b
+    print("Multiplication:", mul)
+
+addition()
+subtraction()
+multiplication()
+
+
